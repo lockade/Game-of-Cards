871f87bd6e6d253dc2598b6156816ff1c8206dc8
lockade <jeffersondias-@hotmail.com>
2019-11-23 17:43:06 -0300

    First Commit

---
+++ Game of Cards/Game of Cards/Jogador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Game_of_Cards
+{
+    class Jogador
+    {
+        public string nome { get; set; }
+        public int jogosVencidos { get; set; }
+        List<Carta> cartasMao = new List<Carta>();
+    }
+}
